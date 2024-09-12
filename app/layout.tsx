f1628aaacc1eb827def69eab34d2e5b8c87fce69
AntonioGally <antonio.gally@gmail.com>
2024-09-11 22:01:54 -0300

    feat: :sparkles: Adding firebase deploy config

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,14 +20,14 @@ export default function RootLayout({
         <Script
           id="tag-analytics"
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-ZPXBMX5697`}
         />
         <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
+            gtag('config', 'G-ZPXBMX5697', {
               page_path: window.location.pathname,
             });
           `}
